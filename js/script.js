711dd415ac1c62c2c8d393eca44de8f1b5eee4d6
JiaLinYang0410 <jlyang5008@gmail.com>
2019-11-13 23:17:34 +0000

    11/13/19

--- js/script.js
+++ js/script.js
@@ -5,13 +5,65 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+var PAPER = "paper"
+var ROCK = "rock"
+var SCISSORS = "scissors"
 
-
+var COMPUTER_WINS = "You lose"
+var USER_WINS = "You win"
+var TIE = "It's a tie"
+ 
 // DOCUMENT READY FUNCTION BELOW
 $("#shoot").click(function(){
-    var input= $("#input").val();
+    var input= $("#input").val().toLowerCase();
+    var computerInput = chooseComputer();
     $("#userChoice").html(input);
+    $("#computerChoice").html(computerInput);
     a= ["rock" , "paper" , "scissors"]
+    var result = choice(computerInput, input);
+    $("#result").html(result);
 });
 
+alert(computer)
+function chooseComputer(){
+    let num= Math.random();
+if(num <= 0.33){
+    return ROCK
+}else if(num > 0.33 && num <=0.66){
+    return SCISSORS
+}else{
+    return PAPER
+}
+}
+function choice(computer , input){
+if(computer == input){
+    return TIE
+}
+if(computer == ROCK){
+    if(input == PAPER){
+        return USER_WINS
+    }
+    if(input == SCISSORS){
+        return COMPUTER_WINS
+    }
+}
+if (computer == PAPER){
+    if(input == ROCK){
+        return COMPUTER_WINS
+    }
+    if(input == SCISSORS){
+        return USER_WINS
+    }
+}
+if(computer == SCISSORS){
+    if(input == ROCK){
+        return USER_WINS
+    }
+    if(input == PAPER){
+        return COMPUTER_WINS
+    }
+    }
+}
 
+
+    
